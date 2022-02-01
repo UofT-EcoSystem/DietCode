760a559643a6015b669ccb45dd0c7397edb95506
ArmageddonKnight <bojian.zheng@mail.utoronto.ca>
2022-02-01 14:06:49 -0500

    Add the auto-scheduler submodule

---
+++ shared/auto_scheduler.py
@@ -0,0 +1,22 @@
+from tvm import auto_scheduler
+
+import os
+import logging
+
+from .logger import remove_log_file
+
+logger = logging.getLogger(__name__)
+
+
+auto_sched_ntrials = int(os.getenv('AUTO_SCHED_NTRIALS', '20'))
+logger.info("Auto-scheduler is doing {} trials".format(auto_sched_ntrials))
+
+
+def get_log_filename(auto_scheduler_name, wkl_name,):
+    log_filename = "{}_autosched_{}.json".format(auto_scheduler_name, wkl_name)
+    remove_log_file(log_filename)
+    return log_filename
+
+_runner_kwargs = {'repeat' : 3, 'min_repeat_ms' : 100, 'timeout' : 10}
+local_rpc_measure_ctx = auto_scheduler.LocalRPCMeasureContext(**_runner_kwargs)
+local_runner = auto_scheduler.LocalRunner(**_runner_kwargs)
