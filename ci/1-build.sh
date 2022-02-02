3f6e0e60588f775a6a80c720a3ec75eb46763162
ArmageddonKnight <bojian.zheng@mail.utoronto.ca>
2022-02-02 01:17:16 -0500

    Add the ci

---
+++ ci/1-build.sh
@@ -0,0 +1,21 @@
+#!/bin/bash -e
+
+PROJECT_ROOT=$(cd $(dirname ${BASH_SOURCE[0]}) && pwd)/..
+
+if [ $1 != "tvm" ] || [ $1 = "tvm_base" ]; then
+        printf "Expecting one command line argument (tvm/tvm_base), but got $1 instead"
+fi
+
+cd ${PROJECT_ROOT}/$1
+
+mkdir -p build && cd build
+
+cmake -DUSE_CUDA=/usr/local/cuda/ \
+      -DUSE_LLVM=/usr/lib/llvm/bin/llvm-config \
+      -DUSE_CUBLAS=1 \
+      -DUSE_CUDNN=1 ..
+make -j 4
+
+cd ${PROJECT_ROOT}/$1/python
+
+python3 setup.py build
