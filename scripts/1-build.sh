0543f4f44c41add5b9ad1fdeb601d4a4195dfc58
ArmageddonKnight <bojian.zheng@mail.utoronto.ca>
2022-02-02 20:47:51 -0500

    Linting

--- scripts/1-build.sh
+++ scripts/1-build.sh
@@ -3,7 +3,8 @@
 PROJECT_ROOT=$(cd $(dirname ${BASH_SOURCE[0]}) && pwd)/..
 
 if [ $1 != "tvm" ] || [ $1 = "tvm_base" ]; then
-        printf "Expecting one command line argument (tvm/tvm_base), but got $1 instead"
+        printf "Expecting one command line argument (tvm/tvm_base), but got $1 instead\n"
+        exit -1
 fi
 
 cd ${PROJECT_ROOT}/$1
