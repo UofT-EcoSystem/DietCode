cf0a240c0093e342ac55e3386b2855dacbd4a8ec
ArmageddonKnight <bojian.zheng@mail.utoronto.ca>
2022-02-22 00:01:36 -0500

    Add the viewcode extension

--- docs/conf.py
+++ docs/conf.py
@@ -35,6 +35,7 @@ extensions = [
     'sphinx.ext.autodoc',
     'sphinx.ext.mathjax',
     'sphinx.ext.githubpages',
+    'sphinx.ext.viewcode'
 ]
 
 # Add any paths that contain templates here, relative to this directory.
